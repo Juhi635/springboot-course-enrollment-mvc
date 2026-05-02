af98aeaf4648f321264dea60b5d8c8c902e9b856
Juhi Dugar <132328037+Juhi635@users.noreply.github.com>
2026-05-03 00:26:43 +0530

    Delete src/main/java/resources directory

--- src/main/java/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO courses (name, instructor) VALUES ('Java', 'Dr Sharma');
-INSERT INTO courses (name, instructor) VALUES ('DBMS', 'Dr Mehta');
-
-INSERT INTO enrollments (student_name, email, course_id)
-VALUES ('Rahul', 'rahul@mail.com', 1);
